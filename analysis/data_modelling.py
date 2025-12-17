460a984cdc32fdb3e7b7196341d045f2ea3847ab
dacelensa <dacelensa00@gmail.com>
2025-12-17 16:19:06 +0100

    add docstring to data modelling functions
    add docstrings and document functions in data modelling module

--- analysis/data_modelling.py
+++ analysis/data_modelling.py
@@ -2,11 +2,36 @@ import pandas as pd
 
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans dataset:
+    - removes duplicates
+
+    :param df: a dataframe to clean
+    :type df: pd.DataFrame
+
+    :return: a cleaned up dataframe
+    """
+        
     # remove duplicates
     df = df.drop_duplicates()
     return df
 
 def changing_columns_name_values(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transforms dataset into data structure:
+    - column names are lowercase and with _ instead of spaces
+    - renames column 'sku_id' to 'product_id'
+    - converts columns 'product_id', 'warehouse_id', and 'supplier_id' to number
+    - converts column 'date' to date
+    - removes 'stockout_flag' column - it has invalid data
+    - replaces missing values with 'Unknown'
+
+    :param df: a dataframe to transform
+    :type df: pd.DataFrame
+
+    :return: a cleaned up dataframe
+    """
+
     # standardize column names
     data.columns = [col.strip().lower().replace(" ", "_") for col in data.columns]
 
